13683f75afbb72e23f76ab192d66b5a69477da27
zhanghongbin01 <zhanghongbin01@zhanghongbin01s-MacBook-Air.local>
2017-09-11 21:36:07 +0800

    update

--- src/redis.c
+++ src/redis.c
@@ -380,6 +380,7 @@ void redisLogRaw(int level, const char *msg) {
 /* Like redisLogRaw() but with printf-alike support. This is the function that
  * is used across the code. The raw version is only used in order to dump
  * the INFO output on crash. */
+//记录redis日志
 void redisLog(int level, const char *fmt, ...) {
     va_list ap;
     char msg[REDIS_MAX_LOGMSG_LEN];
